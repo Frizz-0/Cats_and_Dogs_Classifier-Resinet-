73a28948a73b1ed0b55eb193131d2a732fa7c48b
@FriZz_0 <bharathfrizz@gmail.com>
2025-12-26 19:52:26 +0530

    Cleaned Code and Demo Output

--- src/app.py
+++ src/app.py
@@ -40,17 +40,18 @@ def predict_image(img_path, model):
          # Print probabilities for debugging
         print(f"Probabilities: Cat={probabilities[0][0]:.4f}, Dog={probabilities[0][1]:.4f}")
         label = ("Cat" if pred.item() == 0 else "Dog")
-        confidence = (f'Cat={probabilities[0][0]:.4f}' if label == "Cat" else f"Dog={probabilities[0][1]:.4f}" )
+        confidence = (f'{probabilities[0][0]:.4f}' if label == "Cat" else f"{probabilities[0][1]:.4f}" )
+
         
         return label, confidence
 
-model = load_trained_model(r'\CatsnDogs.pt',num_classes=2)
-# result = predict_image(r'X:\Python\ML\Projects\Cats_and_Dogs\images\03.jpg',model)
+model = load_trained_model(r'models\CatsnDogs.pt',num_classes=2)
+# result = predict_image(r'x:\Python\ML\Projects\Cats_and_Dogs\images\00.jpg',model)
 # print(f'Prediction : {result}')
 
 
+#Streamlit UI
 
-# ---------------- PAGE CONFIG ----------------
 st.set_page_config(
     page_title="Cats vs Dogs AI",
     page_icon="🐾",
@@ -58,7 +59,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-# ---------------- CUSTOM CSS ----------------
+
 st.markdown("""
 <style>
 @keyframes fadeIn {
@@ -85,8 +86,13 @@ html, body {
     font-size: 1.2rem;
     color: #dcdcdc;
     margin-bottom: 30px;
+    animation: fadeIn 1.1s ease-in-out;
 }
 
+.stProgress > div > div > div > div {
+    background-color: #fffff;
+}
+            
 .glass-card {
     background: rgba(255, 255, 255, 0.08);
     backdrop-filter: blur(12px);
@@ -96,84 +102,77 @@ html, body {
 }
 
 .result-cat {
-    color: #00ffcc;
+    color: #f7971e;
     font-size: 2rem;
     font-weight: bold;
+    animation: fadeIn 1s ease-in-out;
+}
+    
+.result-cat-conf {
+    color: #f7971e;
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin-top: 8px;
+    animation: fadeIn 1s ease-in-out;
 }
 
 .result-dog {
     color: #ffcc00;
     font-size: 2rem;
     font-weight: bold;
+    animation: fadeIn 1s ease-in-out;
 }
+
+.result-dog-conf {
+    color: #ffcc00;
+    font-size: 1.5rem;
+    font-weight: bold;
+    animation: fadeIn 1s ease-in-out;
+}
+                       
 </style>
 """, unsafe_allow_html=True)
 
-# ---------------- HEADER ----------------
 st.markdown('<div class="title-text">Cats vs Dogs AI 🐾</div>', unsafe_allow_html=True)
 st.markdown('<div class="subtitle">Upload an image</div>', unsafe_allow_html=True)
 
-# ---------------- MODEL LOADER ----------------
 @st.cache_resource
 def load_model():
-    """
-    Replace this with your actual model loading logic
-    """
     return "your_model_here"
 
-# model = load_model()
-
-# ---------------- PREDICTION FUNCTION ----------------
-def predict(image: Image.Image):
-    """
-    Replace this with your real inference logic
-    """
-    time.sleep(1.2)  # fake latency for realism
-    label = np.random.choice(["Cat", "Dog"])
-    confidence = np.random.uniform(0.75, 0.99)
-    return label, confidence
-
-# ---------------- MAIN CARD ----------------
 with st.container():
-    st.markdown('<div class="glass-card">', unsafe_allow_html=True)
+ 
 
     uploaded_file = st.file_uploader(
-        "📸 Upload an image",
+        "Upload an image",
         type=["jpg", "jpeg", "png"],
         label_visibility="collapsed"
     )
 
     if uploaded_file:
-        # image = Image.open(uploaded_file).convert("RGB")
         image = uploaded_file
         st.image(image, caption="Uploaded Image", width='stretch')
 
 
-        # if st.button("🚀 Predict", use_container_width=True):
-        
-        with st.spinner("AI is thinking..."):
-            label, confidence = predict(image)
-            # label = predict(image)    
+        # if st.button("Predict", use_container_width=True): #Incase if needed a PREDICT Button to predict
 
-
+        with st.spinner("Thinking..."):
+            label, confidence = predict_image(image,model)
+ 
             st.divider()
 
             if label == "Cat":
-                st.markdown(f"<div class='result-cat'>🐱 It's a CAT!</div>", unsafe_allow_html=True)
+                # st.markdown(f"<div class='result-cat'>It's a CAT! </div>", unsafe_allow_html=True)  
+                st.markdown(f"<div style='display: flex; justify-content: space-between;'><span class='result-cat'>It's a CAT!</span><span class='result-cat-conf'>Confidence = {float(confidence):.3f}%</span></div>", unsafe_allow_html=True)
+
             else:
-                st.markdown(f"<div class='result-dog'>🐶 It's a DOG!</div>", unsafe_allow_html=True)
+                # st.markdown(f"<div class='result-dog'>It's a DOG!</div>", unsafe_allow_html=True)
+                st.markdown(f"<div style='display: flex; justify-content: space-between;'><span class='result-dog'>It's a DOG!</span><span class='result-dog-conf'>Confidence = {float(confidence):.3f}%</span></div>", unsafe_allow_html=True)
 
-            st.progress(int(confidence * 100))
-            st.caption(f"Confidence: **{confidence*100:.2f}%**")
 
+            st.progress(float(confidence))
+            # st.markdown(f"<div class='result-cat'>Confidence = {float(confidence):.3f}%</div>", unsafe_allow_html=True,text_alignment="right",)
+            # st.caption(f"Confidence: **{float(confidence):.3f}%**")
 
     else:
         st.info("Drag & drop or upload a cat or dog image 🐕🐈")
-
-    st.markdown('</div>', unsafe_allow_html=True)
-
-# ---------------- FOOTER ----------------
-st.markdown(
-    "<p style='text-align:center; color:gray; margin-top:20px;'>Built with ❤️ using Streamlit</p>",
-    unsafe_allow_html=True
-)
